289a7b306897fbc7fa0a73aa27c49a52fe33d58e
Jesus Vazquez <jesusvazquez@jesuss-mac-mini.lan>
2021-07-24 22:40:58 -0600

    Updates

---
+++ precache-manifest.c546dd9c1dc9c7fca30311b46e798406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658e950341d615ace2201bd461494fdc",
+    "url": "/mielito/index.html"
+  },
+  {
+    "revision": "48f827ac7416d44329b5",
+    "url": "/mielito/static/css/main.0feb6022.chunk.css"
+  },
+  {
+    "revision": "9bb78af78f94e085f1d0",
+    "url": "/mielito/static/js/2.03fd427d.chunk.js"
+  },
+  {
+    "revision": "13438eee4e777095ed4206c277e09443",
+    "url": "/mielito/static/js/2.03fd427d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f827ac7416d44329b5",
+    "url": "/mielito/static/js/main.3edd25f8.chunk.js"
+  },
+  {
+    "revision": "a2126caed837d77a61b7",
+    "url": "/mielito/static/js/runtime-main.b371485d.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/mielito/static/media/shopping-bag.30f07071.svg"
+  }
+]);
